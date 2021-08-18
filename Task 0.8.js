79a43aef5c943cd41775445c59da32a043a76e93
Tshediso <tshedisoboshiana@gmail.com>
2021-08-18 17:41:08 +0200

    Fixed the singular and plural bug

--- Task 0.8.js
+++ Task 0.8.js
@@ -2,15 +2,22 @@ function hoursAndMinutes(num) {
   let hours = Math.floor(num / 60);
   let min = num % 60;
 
-  if(hours == 1 && min == 1) {
+  if(hours == 0 && min == 0) {
+    return `${hours} hour, ${min} minute`;
+  }if(hours == 1 && min == 1) {
+    return `${hours} hour, ${min} minute`;
+  }if(hours == 1 && min == 0) {
     return `${hours} hour, ${min} minute`;
   } else if (hours == 1 && min > 1) {
     return `${hours} hour, ${min} minutes`;
   } else if (hours > 1 && min == 1) {
     return `${hours} hours, ${min} minute`;
-  } else {
+  } else
     return `${hours} hours, ${min} minutes`;
-  }
 }
-console.log(hoursAndMinutes(121));
+
+console.log(hoursAndMinutes(0));
+console.log(hoursAndMinutes(60));
+console.log(hoursAndMinutes(61));
 console.log(hoursAndMinutes(63));
+console.log(hoursAndMinutes(123));
