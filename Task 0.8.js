db0a6079b1750664fc3bf3196713ae6fa312884a
Tshediso <79029080+TshedisoB@users.noreply.github.com>
2021-08-20 13:17:36 +0200

    removed return

--- Task 0.8.js
+++ Task 0.8.js
@@ -3,21 +3,22 @@ function hoursAndMinutes(num) {
   let min = num % 60;
 
   if(hours == 0 && min == 0) {
-    return `${hours} hours, ${min} minutes`;
+    console.log (`${hours} hours, ${min} minutes`);
   }if(hours == 1 && min == 1) {
-    return `${hours} hour, ${min} minute`;
+    console.log(`${hours} hour, ${min} minute`);
   }if(hours == 1 && min == 0) {
-    return `${hours} hour, ${min} minutes`;
+    console.log(`${hours} hour, ${min} minutes`);
   } else if (hours == 1 && min > 1) {
-    return `${hours} hour, ${min} minutes`;
+    console.log(`${hours} hour, ${min} minutes`);
   } else if (hours > 1 && min == 1) {
-    return `${hours} hours, ${min} minute`;
-  } else
-    return `${hours} hours, ${min} minutes`;
+    console.log(`${hours} hours, ${min} minute`);
+  } else if (hours > 1 && min > 1){
+    console.log(`${hours} hours, ${min} minutes`);
+  }
 }
 
-console.log(hoursAndMinutes(0));
-console.log(hoursAndMinutes(60));
-console.log(hoursAndMinutes(61));
-console.log(hoursAndMinutes(63));
-console.log(hoursAndMinutes(123));
+hoursAndMinutes(0);
+hoursAndMinutes(60);
+hoursAndMinutes(61);
+hoursAndMinutes(63);
+hoursAndMinutes(123);
