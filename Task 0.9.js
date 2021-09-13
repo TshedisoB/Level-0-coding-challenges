16111032856587f4220d61b237a9a1ebc9e7395f
Tshediso <79029080+TshedisoB@users.noreply.github.com>
2021-09-13 18:22:11 +0200

    formatted with Prettier

--- Task 0.9.js
+++ Task 0.9.js
@@ -1,6 +1,9 @@
 function vowelsCheck(string) {
   let regEx = /[aeiou]/g;
-  vowels = string.replace(/[^\w\s]|(.)(?=\1)/gi,',').toLowerCase().match(regEx);
+  vowels = string
+    .replace(/[^\w\s]|(.)(?=\1)/gi, ",")
+    .toLowerCase()
+    .match(regEx);
   console.log(`Vowels: ${vowels}`);
 }
 vowelsCheck("TshEEdisoo");
