fa9ecea8ca8acfb3155bc416aed1dadcfbc34660
Tshediso <79029080+TshedisoB@users.noreply.github.com>
2021-09-13 18:13:42 +0200

    formatted with Prettier

--- Task 0.5.js
+++ Task 0.5.js
@@ -1,7 +1,7 @@
-function areaOfTriangle(num1,num2,num3){
-    let semiPerimeter = (num1 + num2 + num3)/2;
-    let sp = semiPerimeter;
-    let area = Math.sqrt(sp*(sp-num1)*(sp-num2)*(sp-num3));
-    return area;
+function areaOfTriangle(num1, num2, num3) {
+  let semiPerimeter = (num1 + num2 + num3) / 2;
+  let sp = semiPerimeter;
+  let area = Math.sqrt(sp * (sp - num1) * (sp - num2) * (sp - num3));
+  return area;
 }
-console.log(areaOfTriangle(4,3,5));
+console.log(areaOfTriangle(4, 3, 5));
