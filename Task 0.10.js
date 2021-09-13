12e1aaa4bbc8cad3c4d22c5fc18da7a7455ce9d1
Tshediso <79029080+TshedisoB@users.noreply.github.com>
2021-09-13 18:34:02 +0200

    formatted with Prettier

--- Task 0.10.js
+++ Task 0.10.js
@@ -1,16 +1,16 @@
-function sameLetters(word1,word2) {
-  let nWord1 = word1.split("");
-  let nWord2 = word2.split("");
-  
-  let res = "";
-  for (let i = 0; i < nWord1.length; i++) {
-    for (let j = 0; j < nWord2.length; j++) {
-      if (nWord1[i] === nWord2[j]) {
-        res += nWord1[i];
+function sameLetters(word1, word2) {
+  let newWord1 = word1.split("");
+  let newWord2 = word2.split("");
+
+  let word3 = "";
+  for (let i = 0; i < newWord1.length; i++) {
+    for (let j = 0; j < newWord2.length; j++) {
+      if (newWord1[i] === newWord2[j]) {
+        word3 += newWord1[i];
       }
     }
   }
-  let refactor = res.replace(/(.)\1+/g,'$1').split("");
-  console.log(`Common letters:  ${refactor}`);
+  let newWord3 = word3.replace(/(.)\1+/g, "$1").split("");
+  console.log(`Common letters:  ${newWord3}`);
 }
 sameLetters("houuse", "compuuuters");
