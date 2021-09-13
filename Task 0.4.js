3280415b0c3a9afe4f2dce80c632b2e082b25749
Tshediso <79029080+TshedisoB@users.noreply.github.com>
2021-09-13 18:09:43 +0200

    formatted with Prettier

--- Task 0.4.js
+++ Task 0.4.js
@@ -1,9 +1,7 @@
-
-function evenOrOdd(num){
-    if(num % 2 == 0){
-        console.log("even")
-    }else
-        console.log("odd")
+function evenOrOdd(num) {
+  if (num % 2 == 0) {
+    console.log("even");
+  } else console.log("odd");
 }
 evenOrOdd(3);
 evenOrOdd(4);
