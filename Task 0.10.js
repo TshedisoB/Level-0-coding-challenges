5e07fe9ff78e7ac5c5cc05e00a8e5774f6fcda95
Tshediso <79029080+TshedisoB@users.noreply.github.com>
2021-09-25 17:36:29 +0200

    removed unnecessary arrays

--- Task 0.10.js
+++ Task 0.10.js
@@ -1,16 +1,13 @@
 function sameLetters(word1, word2) {
-  let newWord1 = word1.split("");
-  let newWord2 = word2.split("");
-
   let word3 = "";
-  for (let i = 0; i < newWord1.length; i++) {
-    for (let j = 0; j < newWord2.length; j++) {
-      if (newWord1[i] === newWord2[j]) {
-        word3 += newWord1[i];
+  for (let i = 0; i < word1.length; i++) {
+    for (let j = 0; j < word2.length; j++) {
+      if (word1[i] === word2[j]) {
+        word3 += word1[i];
       }
     }
   }
-  let newWord3 = word3.replace(/(.)\1+/g, "$1").split("");
-  console.log(`Common letters:  ${newWord3}`);
+  word3 = word3.replace(/(.)\1+/g, "$1").split("");
+  console.log(`Common letters:  ${word3}`);
 }
 sameLetters("houuse", "compuuuters");
