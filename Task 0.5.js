4009ca987441040b9a09129f6f896891f49377fc
Tshediso <79029080+TshedisoB@users.noreply.github.com>
2021-09-25 17:20:39 +0200

    removed extra variable

--- Task 0.5.js
+++ Task 0.5.js
@@ -1,7 +1,8 @@
 function areaOfTriangle(num1, num2, num3) {
-  let semiPerimeter = (num1 + num2 + num3) / 2;
-  let sp = semiPerimeter;
-  let area = Math.sqrt(sp * (sp - num1) * (sp - num2) * (sp - num3));
+  const semiPer = (num1 + num2 + num3) / 2;
+  const area = Math.sqrt(
+    semiPer * (semiPer - num1) * (semiPer - num2) * (semiPer - num3)
+  );
   return area;
 }
 console.log(areaOfTriangle(4, 3, 5));
