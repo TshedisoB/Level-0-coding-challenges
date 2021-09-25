1c9a7196cf43351007c3add88d147b8731672d5d
Tshediso <79029080+TshedisoB@users.noreply.github.com>
2021-09-25 17:25:57 +0200

    changed let to const

--- Task 0.7.js
+++ Task 0.7.js
@@ -1,11 +1,11 @@
 function celsiusToFahrenheit(num) {
-  let celDegree = num * 1.8 + 32;
+  const celDegree = num * 1.8 + 32;
   return celDegree;
 }
 console.log(celsiusToFahrenheit(5));
 
 function fahrenheitToCelsius(num) {
-  let fahDegree = ((num - 32) * 5) / 9;
+  const fahDegree = ((num - 32) * 5) / 9;
   return fahDegree;
 }
 console.log(fahrenheitToCelsius(41));
