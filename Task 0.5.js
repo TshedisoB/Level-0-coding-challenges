dadecacab211ff873ff8c389cf831b92d8154f8e
Tshediso <79029080+TshedisoB@users.noreply.github.com>
2021-09-25 17:50:43 +0200

    replaced return with console.log

--- Task 0.5.js
+++ Task 0.5.js
@@ -3,6 +3,6 @@ function areaOfTriangle(num1, num2, num3) {
   const area = Math.sqrt(
     semiPer * (semiPer - num1) * (semiPer - num2) * (semiPer - num3)
   );
-  return area;
+  console.log(area);
 }
-console.log(areaOfTriangle(4, 3, 5));
+areaOfTriangle(4, 3, 5);
