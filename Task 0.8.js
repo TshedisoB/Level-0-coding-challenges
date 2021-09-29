156798abef469ced593b906cb97f7091bdfe625b
Tshediso <79029080+TshedisoB@users.noreply.github.com>
2021-09-29 03:16:27 +0200

    removed repetitive conditions

--- Task 0.8.js
+++ Task 0.8.js
@@ -1,29 +1,23 @@
 function hoursAndMinutes(num) {
   const hours = Math.floor(num / 60);
   const min = num % 60;
+  let minStr = "minute";
+  let hourStr = "hour";
 
-  if (hours == 0 && min == 0) {
-    console.log(`${hours} hours, ${min} minutes`);
-  } else if (hours == 1 && min == 1) {
-    console.log(`${hours} hour, ${min} minute`);
-  } else if (hours == 1 && min == 0) {
-    console.log(`${hours} hour, ${min} minutes`);
-  } else if (hours == 1 && min > 1) {
-    console.log(`${hours} hour, ${min} minutes`);
-  } else if (hours < 1 && min > 1) {
-    console.log(`${hours} hours, ${min} minutes`);
-  } else if (hours > 1 && min > 1) {
-    console.log(`${hours} hours, ${min} minutes`);
-  } else if (hours > 1 && min == 1) {
-    console.log(`${hours} hours, ${min} minute`);
-  } else if (hours > 1 && min == 0) {
-    console.log(`${hours} hours, ${min} minutes`);
-  } else if (hours > 1 && min > 1) {
-    console.log(`${hours} hours, ${min} minutes`);
+  if (hours == 1) {
+    hourStr;
+  } else if (hours == 0 || hours > 0) {
+    hourStr += "s";
   }
+  if (min == 1) {
+    minStr;
+  } else if (min == 0 || min > 0) {
+    minStr += "s";
+  }
+  console.log(`${hours} ${hourStr}, ${min} ${minStr}`);
 }
 hoursAndMinutes(0);
 hoursAndMinutes(57);
 hoursAndMinutes(60);
-hoursAndMinutes(66);
+hoursAndMinutes(61);
 hoursAndMinutes(120);
